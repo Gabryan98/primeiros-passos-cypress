af2e4fe6aea67f96cf9bb715758d39ad36fa520b
Gabryan98 <gabrielbryan98@gmail.com>
2025-04-29 22:21:10 -0300

    First commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,18 @@
+describe('Orange HRM Tests', () => {
+  it.skip('Login - Succsess', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+  })
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('test123')
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal','/web/index.php/auth/login')
+    cy.get('.oxd-alert')
+  })
+})
