3df238b2fc8e9b6d2baf197b167361d971d382ad
Gabryan98 <gabrielbryan98@gmail.com>
2025-04-30 23:07:10 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,18 +1,28 @@
 describe('Orange HRM Tests', () => {
-  it.skip('Login - Succsess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-    cy.get('.oxd-button').click()
+  
+  const selectorList = {
+    urlField: "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    selectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
+    wrongCredentialAlert: "[role='alert']"
+  }
+  
+  it('Login - Succsess', () => {
+    cy.visit(selectorList.urlField)
+    cy.get(selectorList.usernameField).type('Admin')
+    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/dashboard/index')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+    cy.get(selectorList.selectionTitleTopBar).contains('Dashboard')
   })
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Test')
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('test123')
-    cy.get('.oxd-button').click()
+    cy.visit(selectorList.urlField)
+    cy.get(selectorList.usernameField).type('Test')
+    cy.get(selectorList.passwordField).type('test123')
+    cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal','/web/index.php/auth/login')
-    cy.get('.oxd-alert')
+    cy.get(selectorList.wrongCredentialAlert)
   })
 })
