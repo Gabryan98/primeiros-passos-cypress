f8776b9a7a184b6c4e04cc82f05b5c20311d88cc
Gabryan98 <gabrielbryan98@gmail.com>
2025-05-03 23:46:54 -0300

    Project Cypress Update 03-05

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,50 @@
+import userData from '../fixtures/user-data.json'
+
+describe('Orange HRM Tests', () => {
+  
+  const selectorList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    selectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
+    dashboardGrid: ".orangehrm-dashboard-grid",
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dateField: "[placeholder='yyyy-mm-dd']",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']"
+  }
+  
+  it.only('User Info Update - Success', () => {
+    cy.visit("/auth/login")
+    cy.get(selectorList.usernameField).type(userData.usernameSuccess.username)
+    cy.get(selectorList.passwordField).type(userData.usernameSuccess.password)
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get(selectorList.dashboardGrid)
+    cy.get(selectorList.myInfoButton).click()
+    cy.get(selectorList.firstNameField).clear().type("FirstNameTest")
+    cy.get(selectorList.lastNameField).clear().type("LastNameTest")
+    //cy.get(selectorList.genericField).eq().clear().type("NickNameTest")
+    cy.get(selectorList.genericField).eq(3).clear().type("123456789")
+    cy.get(selectorList.genericField).eq(4).clear().type("OtherIdTest")
+    cy.get(selectorList.genericField).eq(5).clear().type("LicenseTest")
+    cy.get(selectorList.genericField).eq(6).clear().type('2025-05-03')
+    cy.get(selectorList.dateCloseButton).click()
+    cy.get(selectorList.submitButton).eq(0).click()
+    cy.get('body').should('contain','Successfully Updated')
+    //cy.get(selectorList.genericField).eq(8).clear().type("sinNumberTest")
+
+  })
+  it('Login - Fail', () => {
+    cy.visit("/auth/login")
+    cy.get(selectorList.usernameField).type(userData.userFail.username)
+    cy.get(selectorList.passwordField).type(userData.userFail.password)
+    cy.get(selectorList.loginButton).click()
+    cy.location('pathname').should('equal','/web/index.php/auth/login')
+    cy.get(selectorList.wrongCredentialAlert)
+  })
+})
